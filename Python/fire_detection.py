3f8510eafe80db19ce97feab9c440c5e8ac84b06
Marvin <cruzmarvin447@gmail.com>
2026-03-03 22:25:20 -0800

    Added an outline detailing the production of the fire alarm script for the FireCracker project

---
+++ Python/fire_detection.py
@@ -0,0 +1,14 @@
+import serial 
+import numpy
+import matplotlib as plt
+
+# Get data from the MCU 
+
+# Keep most of the logic, fire detection logic on Python
+
+# Connect to the receiver, get data from MCU via Serial then test bluetooth capabilities
+
+# Utilizing that data from the MCU, estimate whether we have a fire. Utilize thresholds to determine if the readings near a node are too high. 
+
+# Get the data from each node. I want to pass in data from each node object, if the data is abnormal for that node send it a signal. Flash an LED there is a fire there 
+
