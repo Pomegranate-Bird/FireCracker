962891969a73a581b9a52480d7e2835c062bb3fc
Marvin <cruzmarvin447@gmail.com>
2026-03-07 02:23:06 -0800

    Ignore Arduino libraries folder

--- Ardunio/libraries/bmv080-sdk-v11-2-0/apps/web_app_bst_application_board/bmv080/drawer.js
+++
@@ -1,5 +0,0 @@
-const drawer = mdc.drawer.MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
-const topAppBar = mdc.topAppBar.MDCTopAppBar.attachTo(document.querySelector('#app-bar'));
-topAppBar.listen('MDCTopAppBar:nav', () => {
-drawer.open = !drawer.open;
-});
